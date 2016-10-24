b69ad6ef534143af6dd59ef2a7467985663f55c7
Arjun Baru <arjun.baru2@gmail.com>
2016-10-24 15:48:32 +0530

    Initial Commit

---
+++ virtualWorld2Tests/virtualWorld2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  virtualWorld2Tests.swift
+//  virtualWorld2Tests
+//
+//  Created by Arjun Baru on 24/10/16.
+//  Copyright © 2016 Arjun Baru. All rights reserved.
+//
+
+import XCTest
+@testable import virtualWorld2
+
+class virtualWorld2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
